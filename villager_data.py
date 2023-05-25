ad4c40a860f7b10909061bff428832d3fe6c3d0a
Minnu John <minnu.john@snhu.edu>
2023-05-25 12:58:25 -0500

    function all_data complete

--- villager_data.py
+++ villager_data.py
@@ -53,10 +53,25 @@ def all_names_by_hobby(filename):
     Return:
         - list[list[str]]: a list of lists containing names
     """
-
+    
     # TODO: replace this with your code
+    hobby_name = []
+    for i in range(6):
+        lst = []
+        hobby_name.append(lst)
 
-    return []
+    cnt = 0
+    names = []
+    file = open(filename)
+    hobby_list=['Fitness','Nature','Education','Music','Fashion','Play']
+    for line in file:
+        word_list = line.split('|')
+        hobby = word_list[3]
+        name = word_list[0]
+        idx=hobby_list.index(hobby)
+        hobby_name[idx].append(name)
+
+    return hobby_name
 
 
 def all_data(filename):
@@ -75,7 +90,10 @@ def all_data(filename):
     all_data = []
 
     # TODO: replace this with your code
-
+    file = open(filename)
+    for line in file:
+        word_list = line.rstrip().split("|")
+        all_data.append(tuple(word_list))
     return all_data
 
 
@@ -94,7 +112,13 @@ def find_motto(filename, villager_name):
     """
 
     # TODO: replace this with your code
-
+    file = open(filename)
+    for line in file:
+        words = line.split('|')
+        name = words[0]
+        motto = words[4]
+        if name == villager_name:
+            return motto
 
 def find_likeminded_villagers(filename, villager_name):
     """Return a set of villagers with the same personality as the given villager.
@@ -113,4 +137,3 @@ def find_likeminded_villagers(filename, villager_name):
 
     # TODO: replace this with your code
 
-print(all_species("villagers.csv"))
